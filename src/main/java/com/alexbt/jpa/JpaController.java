eb8fb96eec3ae3aeda15bc2c36347555e34c0fce
Mostafa <m.teba1991@gmail.com>
2017-12-12 11:52:43 -0800

    ddd

--- src/main/java/com/alexbt/jpa/JpaController.java
+++ src/main/java/com/alexbt/jpa/JpaController.java
@@ -20,7 +20,7 @@ public class JpaController {
 		return modelJpaRepository.findAll();
 	}
 	
-	@RequestMapping(value = "/repo/{value}", method = RequestMethod.GET)
+	@RequestMapping(path = "/repo/{value}", method = RequestMethod.GET)
 	public void saveByRepo(@PathVariable String value) {
 		Model model = new Model();
 		model.setId(System.currentTimeMillis());
